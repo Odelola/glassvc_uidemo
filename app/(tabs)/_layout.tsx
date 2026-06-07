b9f83490a8a16288f649740ce65c015d0af412a1
Odelola <integralandintegrations@gmail.com>
2026-06-07 23:59:33 +0100

    chore: finishing bottom tab ui

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -87,7 +87,6 @@ export default function TabLayout() {
         listeners={{
           tabPress: (e) => {
             e.preventDefault();
-            console.log('tabPress');
           }
         }}
       /> */}
